498fa4326661c4e94ca40c5406325df95fb63ac8
Scheleder <scheleder@yahoo.com>
2023-01-26 14:42:43 -0300

    edited copyrigth

--- carreer.php
+++ carreer.php
@@ -134,7 +134,7 @@
                 </div>            
             </main>
          <footer>
-         <a href="contact.php"><p>João Scheleder Neto &copy; 2022</p></a>   
+         <a href="contact.php"><p>João Scheleder Neto &copy; 2023</p></a>   
          </footer>
          <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script> 
