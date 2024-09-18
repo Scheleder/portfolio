603213177d30c2488db885c971618e2978218f8b
João Scheleder Neto <me@scheleder.com>
2024-09-18 16:42:37 -0300

    adicionado estoque-facil

--- projects.php
+++ projects.php
@@ -60,9 +60,14 @@
         </header>
              <main>
                 <div id="container-fluid">
+
                     <h1>Projetos e Realização Profissional</h1> 
-                    
-                    <h4><ion-icon size="normal" name="create"></ion-icon> APLICAÇÃO ERP - CETUS SOLUTION</h4>
+
+                    <h4><ion-icon size="normal" name="code-working"></ion-icon> APLICAÇÃO WEB - CONTROLE DE ESTOQUE</h4>
+                    <h5>Sistema completo de gestão de estoque, com possibilidade de cadastro de vários armazens e com integração com inteligênica artificial. 
+                        Composto por uma API em nodejs com documentação em SWAGGER e um front-end em REACTJS.</h5>
+
+                    <h4><ion-icon size="normal" name="sparkles"></ion-icon> APLICAÇÃO ERP - CETUS SOLUTION</h4>
                     <h5>Sistema de gestão empresarial, atuação em diversos módulos, principlamente em produção e qualidade.</h5>
 
                     <h4><ion-icon size="normal" name="create"></ion-icon> APLICAÇÃO WEB - GERENCIADOR DE AGENDAMENTOS</h4>
